368798b7ae8d981247fc7c5ce30d01d323d174fc
ZGS <zgstowell@gmail.com>
2021-04-19 20:48:46 -0700

    adding all current work to new repo. gotta get those commit numbers up, man!

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CatWorx.BadgeMaker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Employee> employees = GetEmployeeInfo();
+            Util.PrintEmployees(employees);
+            Util.MakeCSV(employees);
+        }
+        static List<Employee> GetEmployeeInfo()
+        {
+            List<Employee> employees = new List<Employee>();
+            while (true)
+            {
+                Console.WriteLine("Please enter First Name: ");
+                // Get first name
+                string empFirstName = Console.ReadLine();
+
+                Console.WriteLine("Please enter Last Name: ");
+                // Get last name
+                string empLastName = Console.ReadLine();
+
+                Console.WriteLine("Please enter Photo URL: ");
+                // Get photo url
+                string empPhotoUrl = Console.ReadLine();
+
+                Console.WriteLine("Please enter Employee ID: ");
+                // Get employee id (and convert to integer)
+                int empId = Int32.Parse(Console.ReadLine());
+
+
+                // Create a new Employee instance
+                Employee employee = new Employee(empFirstName, empLastName, empId, empPhotoUrl);
+                employees.Add(employee);
+
+                Console.WriteLine("Enter another employee? (y/n):");
+                // run again?
+                string goAgain = Console.ReadLine();
+                // Break if the user enters 'n'
+                if (goAgain == "n")
+                {
+                    break;
+                }
+
+            }
+            return employees;
+        }
+    }
+}
---
+++ Util.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace CatWorx.BadgeMaker
+{
+    class Util
+    {
+        public static void PrintEmployees(List<Employee> employees)
+        {
+            for (int i = 0; i < employees.Count; i++)
+            {
+                string template = "{0,-10}\t{1,-20}\t{2}";
+                Console.WriteLine(String.Format(template, employees[i].GetId(), employees[i].GetName(), employees[i].GetPhotoUrl()));
+            }
+        }
+
+        public static void MakeCSV(List<Employee> employees)
+        {
+            if (!Directory.Exists("data"))
+            {
+                Directory.CreateDirectory("data");
+            }
+
+            using (StreamWriter file = new StreamWriter("data/employees.csv"))
+            {
+                file.WriteLine("ID,Name,PhotoUrl");
+                // Loop over employees
+                for (int i = 0; i < employees.Count; i++)
+                {
+                    // Write each employee to the file
+                    string template = "{0},{1},{2}";
+                    file.WriteLine(String.Format(template, employees[i].GetId(), employees[i].GetName(), employees[i].GetPhotoUrl()));
+                }
+            }
+        }
+
+        public static void MakeBadges(List<Employee> employees)
+        {
+
+        }
+
+
+    }
+}
